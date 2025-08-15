80151b7e5073a1193e3c6ee4f69746edb07dc3b3
teetusp <tee.22182@gmail.com>
2025-08-15 12:15:49 +0700

    ลองจ้า ใจเย็นๆ

---
+++ mypthon/Test05.py
@@ -0,0 +1,12 @@
+fullname=input('ป้อนชื่อ-นามสกุล:')
+year_born=input('ป้อนปีเกิด พ.ศ.:')
+print('----------------')
+print(f'สวัสดีคุณ {fullname}')
+print(f'ุคุณเกิดปี พ.ศ. {year_born} ตอนนี้คุณอายุ {2568 - int(year_born)}')
+print('----------------')
+print(f'{fullname} เกิดปี พ.ศ. {year_born} ตอนนี้คุณอายุ {2568 - int(year_born)}')
+print()
+print()
+print('-------------')
+
+

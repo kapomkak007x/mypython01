8cdc76f2ba79a0f095812eb85f7743bf493e0554
kapomkak007x <vdgjcmicc2@gmail.com>
2025-09-05 11:59:24 +0700

    Test

---
+++ mypthon/Test27.py
@@ -0,0 +1,21 @@
+# No parameter no return
+# parameter คือตัวเเปรประเภทหนึ่ง ที่ใช้เฉพาะในฟังก์ชัน เท่านั้น
+# return คือการส่งค่ากลับจากฟังก์ชัน
+'''
+def fubc_name():
+   คำสั่ง
+   คำสั่ง
+   .....
+'''
+def showHI():
+    print("Hi....")
+
+print('sawasdee')
+
+def showBye():
+    print("Bye....")
+
+print('HI')
+
+showHI()
+showBye()
---
+++ mypthon/Test28.py
@@ -0,0 +1,16 @@
+def showHi(your_name):
+    print('---------------------------')
+    print(f'สวัสดี คุณ {your_name}')
+    print('---------------------------')
+
+
+def sumnum(num1, num2):
+    print('---------------------------')
+    print(f'ผลบวกของ {num1} กับ {num2} = {num1+num2}')
+    print('---------------------------')
+
+
+showHi('Seksan')
+showHi('Somchai')
+sumnum(10, 20)
+showHi('Somsri')
